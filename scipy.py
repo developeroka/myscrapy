574bb18c4fe38904ba883cb5a24e262d98ecaff9
developeroka <developeroka@gmail.com>
2019-04-09 21:12:42 +0430

    get price of each event in main page

--- scipy.py
+++ scipy.py
@@ -31,5 +31,6 @@ if __name__ == '__main__':
             event_name = link.a.findChildren()[4].text
             event_place = link.a.findChildren()[7].text
             event_datetime = link.a.findChildren()[3].findChildren()[8].text
+            event_price = link.a.findChildren()[3].findChildren()[11].text
             # writer = csv.writer(csv_file)
             # writer.writerow([event_name, event_price, event_place, event_date, event_time, event_link])
